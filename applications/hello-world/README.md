13ad428a38b9e049c8c134c7471cb9ac607766ad
Sako <sako@opengov.com>
2021-12-06 01:28:36 -0800

    updates

---
+++ applications/hello-world/README.md
@@ -0,0 +1,5 @@
+
+```
+docker build -t sakomws/hello-world:0.1.0 .
+docker push sakomws/hello-world:0.1.0
+```
--- applications/hello-world/app/app.py
+++ applications/hello-world/app/app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello from YOU!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
